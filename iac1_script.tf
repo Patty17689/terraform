29e28778fc4eae8decee214a77f4c884fcfb49b8
Patty17689 <166388263+Patty17689@users.noreply.github.com>
2024-08-30 11:20:37 +0530

    Delete iac1_script.tf

--- iac1_script.tf
+++
@@ -1,9 +0,0 @@
-# Configure the AWS provider
-provider "aws" {
-  region = "us-east-1"  # You can change this to your preferred region
-}
-
-# Create an S3 bucket
-resource "aws_s3_bucket" "patdemo" {
-  bucket = "patdemo"
-}
